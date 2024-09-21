3c2df589128975208a1c701c66bdfabf65f4d8f9
Kaan <kaan.sahin2@hotmail.com>
2024-09-21 15:50:01 +0300

    Setup ApplicationDbContext

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+{
+    
+}
+}
