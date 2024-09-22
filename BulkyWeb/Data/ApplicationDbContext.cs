4aee04c2257b9a894787ee58ad7982782f3c10ca
Kaan <kaan.sahin2@hotmail.com>
2024-09-22 12:52:19 +0300

    Create Category Table

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Data;
+using BulkyWeb.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data;
 
 public class ApplicationDbContext : DbContext
 {
-public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
     
-}
+    }
+
+    public DbSet<Category> Categories { get; set; }
 }
