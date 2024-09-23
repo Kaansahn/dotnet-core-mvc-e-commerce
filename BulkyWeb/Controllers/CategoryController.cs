bfec0582ed7e7ea3baa78fe53f0b0ad6e80b6c44
Kaan <kaan.sahin2@hotmail.com>
2024-09-23 11:30:02 +0300

    TempData

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -37,6 +37,10 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+
+                TempData["success"] = "Category created successfully!";
+
+
                 return RedirectToAction("Index");
             }
             return View();
@@ -66,6 +70,9 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+
+                TempData["success"] = "Category updated successfully!";
+
                 return RedirectToAction("Index");
             }
             return View();
@@ -99,6 +106,8 @@ namespace BulkyWeb.Controllers
 
             _db.Categories.Remove(categoryFromDb);
             _db.SaveChanges();
+
+            TempData["success"] = "Category deleted successfully!";
 
             return RedirectToAction("Index");
         }
