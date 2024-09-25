55f77069322f4b3b0e314882cb74c386f5877e36
Kaan <kaan.sahin2@hotmail.com>
2024-09-25 12:23:55 +0300

    Display all categories

--- BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,24 @@
-using Microsoft.AspNetCore.Mvc;
+using BulkyWebRazor_Temp.Models;
+using BulkyWebRazor_Temp.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
