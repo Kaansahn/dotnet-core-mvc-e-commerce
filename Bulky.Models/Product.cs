8d641a3d1e159e2a3176e0693add44429eba80a4
Kaan <kaan.sahin2@hotmail.com>
2024-09-30 17:35:26 +0300

    Add Image Url Column

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -46,4 +46,6 @@ public class Product
     public int CategoryId { get; set; }
     [ForeignKey("CategoryId")]
     public Category Category { get; set; }
+
+    public string ImageUrl { get; set; }
 }
