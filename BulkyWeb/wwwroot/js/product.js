fa57b1049349c2a393b66c5362efe07b69a4c7b5
Kaan <kaan.sahin2@hotmail.com>
2024-10-05 23:56:10 +0300

    Delete Product

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -11,15 +11,15 @@ function loadDataTable() {
             { data: 'listPrice', "width": "10%" },
             { data: 'author', "width": "15%" },
             { data: 'category.name', "width": "10%" },
-            { data: 'id', 
-                "render": function(data) {
+            { data: 'id',
+                "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
-                     <a class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                     <a href="/admin/product/delete/${data}" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
                 "width": "25%"
-                }
+            }
         ]
     });
 }
